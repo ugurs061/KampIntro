f3ac846a3168592c4d3ad539dc16b9a8fbadd8d7
Uğur <ugur.saglam@arksoft.com.tr>
2022-09-12 15:00:09 +0300

    Constructors

---
+++ Constructors/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Cryptography;
+
+Customer customer1 = new Customer {Id=1,FirstName= "Uğur",LastName="Demiroğ", City= "Trabzon"};
+Customer customer2 = new Customer(2,"emir","c","Ankara");// normal parantez method mantığı
+Console.WriteLine(customer2.FirstName);
+
+
+class Customer // Class içinde özellik tanımladım.
+{
+    public Customer()// default constructor
+    {
+
+    }
+    public Customer(int id, string firstName, string lastName, string city)
+    {
+        Id = id;
+        FirstName = firstName;
+        LastName = lastName;
+        City = city;
+
+    }
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string  City { get; set; }
+}
