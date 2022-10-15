5bd263fdc47a0f801e7fbe981afb8d64fe058ffc
Ugur <rextsts@hotmail.com>
2022-10-15 15:22:34 +0300

    1

--- GenericsIntro/MyList.cs
+++ GenericsIntro/MyList.cs
@@ -7,14 +7,15 @@ namespace GenericsIntro
     class MyList<T>
     {
         T[] items;
-        //constructor
+        
+        // constructor = class new'lenince ilk çalışacak olan metod
         public MyList()
         {
             items = new T[0];
         }
         public void Add(T item)
         {
-            T[] tempArray = items;
+            T[] tempArray = items;// geçici dizi
             items = new T[items.Length + 1];
             for (int i = 0; i < tempArray.Length; i++)
             {
