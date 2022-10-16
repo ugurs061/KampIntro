8d67bf4dda2e9103a986c5673fbd39db47dd1444
Ugur <rextsts@hotmail.com>
2022-10-16 17:00:28 +0300

    16.10.2022

--- LinqProject/Program.cs
+++ LinqProject/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Security.Cryptography.X509Certificates;
+
 List<Category> categories = new List<Category>
 {
    new Category{CategoryId=1, CategoryName="Bilgisayar"},
@@ -14,8 +16,69 @@ List<Product> products = new List<Product>
     new Product{CategoryId=5, ProductId=2, ProductName="Apple Telefon", QuantityPerUnit="4 gb ram", UnitPrice=8000, UnitsInStok=0 }
 };
 
-Console.WriteLine("Hello, World!");
 
+/*
+var result = products.Where(x => x.UnitPrice > 5000 && x.UnitsInStok > 3);// Where; koşulda eşleşen verileri liste tipinde tutar.
+foreach (var product in result)
+{
+    Console.WriteLine(product.ProductName);
+}
+
+static List<Product> GetProductsLinq(List<Product> products) 
+{
+    return products.Where(x => x.UnitPrice > 5000 && x.UnitsInStok > 3).ToList();
+}
+*/
+
+/*
+var result = products.Any(x => x.ProductName == "Acer Laptop");// Any; var mı ? sorunu sorar ve true/false olarak geri döndürür.
+Console.WriteLine(result);,
+*
+
+/*
+var result = products.Find(p => p.ProductId == 1); //Find; şarta uyan nesneyi döndürür.
+Console.WriteLine(result.ProductName);
+*/
+
+/*
+var result = products.FindAll(x=> x.ProductName.Contains("top")); // FindAll; o şarta uyan bütün elemanları getir. liste tipinde döner
+Console.WriteLine(result);      
+*/
+
+// var result = products.Where(x => x.UnitPrice > 5000 && x.UnitsInStok > 3).OrderByDescending(x => x.UnitPrice).ThenByDescending(x=> x.ProductName); // OrderByDes; tersten sırala, ThenByDescending; ondan sonra tersten bunu sırala anlamına gelir.
+
+/*
+var result = from p in products
+             where p.UnitPrice>6000
+             orderby p.UnitPrice descending, p.ProductName ascending
+             select new ProductDto {ProductId=p.ProductId,ProductName=p.ProductName,UnitPrice=p.UnitPrice };
+
+foreach (var product in result)
+{
+    Console.WriteLine(product.ProductName);
+}
+*/
+
+
+
+var result = from p in products
+             join c in categories
+             on p.CategoryId equals c.CategoryId
+             select new ProductDto { ProductId=p.ProductId,CategoryName= c.CategoryName ,ProductName=p.ProductName, UnitPrice=p.UnitPrice };
+
+foreach (var productDto in result)
+{
+    Console.WriteLine(productDto.ProductName + "-" + productDto.CategoryName);
+}
+
+
+class ProductDto
+{
+    public int ProductId { get; set; }
+    public string CategoryName { get; set; }
+    public string ProductName { get; set; }
+    public decimal UnitPrice { get; set; }
+}
 class Product
 {
     public int ProductId { get; set; }
